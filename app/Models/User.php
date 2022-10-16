deccdbef640d026397c239f5d686351ac5335f76
quyhoangs <quyromutc@gmail.com>
2022-10-16 12:34:54 +0700

    Xử lý hiển thị Avatar random theo API Gravatar và Các API ảnh do Laracart cung cấp

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,30 @@ class User extends Authenticatable
     {
         return $this->hasMany(Idea::class);
     }
+
+        public function getAvatar()
+        {
+            //xử lý avt theo gravatar hiển thị random và cố định nó theo email của user
+            //0-9 => 1
+            //a-z => 1-26
+
+            //1.Lấy ký tự đầu tiên của email
+            $firstCharacter = $this->email[0];
+
+            if (is_numeric($firstCharacter)) {
+                //2. Chuyển ký tự thành một số, sử dụng ord ()
+                //3. Lấy số trừ đi 21 (nếu ký tự đầu tiên là một số)
+                $integerToUse = ord(strtolower($firstCharacter)) - 21;
+            } else {
+                //4. Lấy số trừ đi 96 (nếu ký tự đầu tiên là một chữ cái)
+                $integerToUse = ord(strtolower($firstCharacter)) - 96;
+            }
+            //Gravatar Hash theo email của user để lấy avt
+            return 'https://www.gravatar.com/avatar/'
+                .md5($this->email)
+                .'?s=200'
+                .'&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-'
+                .$integerToUse
+                .'.png';
+        }
 }
